dc55b9e89a2ca401eef5d42d97c7574813a4b25f
neitpam <neitpam@gmail.com>
2019-11-18 15:45:15 -0600

    Create yourHours.py

---
+++ yourHours.py
@@ -0,0 +1,5 @@
+# This program multiplies your hours worked times your hourly rate to determine your pay
+hrs = input('Enter hours:')
+rate = input('Enter rate:')
+pay = float(hrs) * float(rate)
+print ('Pay:',pay)
