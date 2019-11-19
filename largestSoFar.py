274987e81cead6d4429812feeae731df1f99334a
neitpam <neitpam@gmail.com>
2019-11-19 10:34:37 -0600

    Create largestSoFar.py

---
+++ largestSoFar.py
@@ -0,0 +1,8 @@
+largestSoFar = -1
+print('Before', largestSoFar)
+for theNum in [9, 41, 12, 3, 74, 15] :
+    if theNum > largestSoFar :
+        largestSoFar = theNum
+    print(largestSoFar, theNum)
+
+print('After' , largestSoFar)
